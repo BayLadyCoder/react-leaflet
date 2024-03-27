1bb0be7af6bc913464df254e8481aef38323e1f3
BayC <rachada_32@hotmail.com>
2024-03-26 22:42:02 -0400

    Show city data with Markers

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,20 +1,36 @@
 import React from 'react';
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 
+import { cities } from '../../data/cities';
+
+const MarkerLayer = ({ coordinates }) => {
+  return (
+    <Marker position={coordinates}>
+      <Popup>
+        A pretty CSS3 popup. <br /> Easily customizable.
+      </Popup>
+    </Marker>
+  );
+};
+
 const Map = () => {
-  const position = [51.505, -0.09];
+  const position = [0, 0];
 
   return (
-    <MapContainer center={position} zoom={13} scrollWheelZoom={false}>
+    <MapContainer center={position} zoom={1} scrollWheelZoom={false}>
       <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
       />
-      <Marker position={position}>
-        <Popup>
-          A pretty CSS3 popup. <br /> Easily customizable.
-        </Popup>
-      </Marker>
+      {cities.features.map((feature) => {
+        const { coordinates } = feature.geometry;
+        return (
+          <MarkerLayer
+            key={coordinates.toString()}
+            coordinates={[coordinates[1], coordinates[0]]}
+          />
+        );
+      })}
     </MapContainer>
   );
 };
